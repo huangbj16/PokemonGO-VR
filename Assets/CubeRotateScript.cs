6603847f314efc95949185552e319de789d24030
huangbj16 <huangbj16@mails.tsinghua.edu.cn>
2019-12-06 16:06:20 +0800

    initial commit

---
+++ Assets/CubeRotateScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotateScript : MonoBehaviour
+{
+    Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float x;
+    // Update is called once per frame
+    void Update()
+    {
+        transform = GetComponent<Transform>();
+        x += Time.deltaTime * 10;
+        transform.rotation = Quaternion.Euler(x, 0, 0);
+    }
+}
---
+++ Assets/Scripts/GazeButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void turnWhite() {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void turnRed() {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void turnBlue() {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
