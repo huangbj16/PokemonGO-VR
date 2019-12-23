1ad29f3fad2f58f3a2c04c69eb1a03a0409d98ab
AlexFxw <alex93179@gmail.com>
2019-12-23 17:11:31 +0800

    Add collision of ruin objects in the forest ruin scene. #4

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Rigidbody boundingBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boundingBox = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        boundingBox.velocity = Vector3.zero;
+    }
+}
