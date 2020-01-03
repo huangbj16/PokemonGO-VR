2c79c7bcb79586d1e6e17a5f0eebb9904273f8f9
AlexFxw <alex93179@gmail.com>
2020-01-03 23:47:44 +0800

    Doing the battle scene.

---
+++ Assets/Scripts/PokemonBattle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonBattle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collide!");
+        if(Caught())
+        {
+            Debug.Log("Caught!");
+        }
+    }
+
+    private bool Caught()
+    {
+        // FIXME: Always true now.
+        float t = Random.value;
+        return t < 2.0f;
+    }
+}
