16fab96e797cb75aaa63194c5a40e0036dc22377
Arthur Yu <arthuryzy329@gmail.com>
2020-01-07 18:14:29 +0800

    add random walk script and energy bar

---
+++ Assets/Scripts/PowerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraFacing : MonoBehaviour {   //挂在需要看向摄像机的UI物体上（例如血条，伤害冒字）
+ 
+    private Camera refCamera;
+    public bool reverFace = false;
+    private Transform mRoot;
+ 
+    private void Awake()
+    {
+        if (!refCamera)
+        {
+            refCamera = Camera.main;
+        }
+        mRoot = transform;
+    }
+ 
+    private void Update()
+    {
+        Vector3 targetPos = mRoot.position + refCamera.transform.rotation * (reverFace?Vector3.back:Vector3.forward);
+        Vector3 targetOrientation = refCamera.transform.rotation * Vector3.up;
+        mRoot.LookAt(targetPos, targetOrientation);
+    }
+
+}
