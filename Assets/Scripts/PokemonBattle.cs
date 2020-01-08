e373b28bbd9a4852cb9c773bac52e9ee4a4970f1
AlexFxw <alex93179@gmail.com>
2020-01-08 12:22:17 +0800

    Fix a bug in scene switching.

--- Assets/Scripts/PokemonBattle.cs
+++ Assets/Scripts/PokemonBattle.cs
@@ -9,7 +9,7 @@ public class PokemonBattle : MonoBehaviour
     public GameObject result;
     public GameObject ball;
     private bool caught;
-    public int aliveFrame = 300;
+    public int aliveFrame = 240;
     private int curFrame;
     //public GameObject ball;
     public GameObject effect;
@@ -20,12 +20,20 @@ public class PokemonBattle : MonoBehaviour
     {
         curFrame = 0;
         caught = false;
-        Vector3 effectPos = gameObject.GetComponent<Transform>().position + new Vector3(0, 1.0f, 0);
+        Vector3 effectPos = gameObject.GetComponent<Transform>().position - new Vector3(0, 2.0f, 1.0f);
+        Vector3 resultPos = gameObject.GetComponent<Transform>().position + new Vector3(0, 1.0f, -1.0f);
         Quaternion effectQuat = effect.GetComponent<Transform>().rotation;
+        Quaternion resultQuat = result.GetComponent<Transform>().rotation;
         effect.GetComponent<Transform>().SetPositionAndRotation(effectPos, effectQuat);
+        result.GetComponent<Transform>().SetPositionAndRotation(resultPos, resultQuat);
         effect.SetActive(false);
         result.SetActive(false);
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+   }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -37,7 +45,12 @@ public class PokemonBattle : MonoBehaviour
                 UnityEngine.Debug.Log("Caught!");
                 result.SetActive(true);
                 effect.SetActive(true);
-                gameObject.SetActive(false);
+                Renderer[] rd = GetComponentsInChildren<Renderer>();
+                foreach (Renderer r in rd)
+                {
+                    r.enabled = false;
+                }
+                UnityEngine.Debug.Log("hide");
                 counterAfterCollision cAC = manager.GetComponent<counterAfterCollision>();
                 cAC.startCounter();
             }
